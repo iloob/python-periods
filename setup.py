b42e68a40594cd3847e8e020ab1a426911aea44f
Olof Sjöbergh <olofsj@gmail.com>
2014-11-27 15:32:32 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="python-periods",
-    version="0.1.2",
+    version="0.1.3",
     description="Some convenient classes and methods for working with time periods",
     author="Johanna Eriksson",
     author_email="johanna.eriksson@booli.se",
